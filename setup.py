5cc411dff69d3dde4cc5e0f3b77322ff3490aec5
ashishhalade01 <58586226+ashishhalade@users.noreply.github.com>
2020-04-01 14:09:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 '''Factorial program'''
 NUM = 7
-factorial = 1
+FACTORIAL = 1
 if NUM < 0:
     print("Sorry, factorial does not exist for negative numbers")
 elif NUM == 0:
     print("The factorial of 0 is 1")
 else:
     for i in range(1, NUM + 1):
-        factorial = factorial*i
-   print("The factorial of", NUM, "is", factorial)
+        FACTORIAL = FACTORIAL*i
+    print("The factorial of", NUM, "is", FACTORIAL)
